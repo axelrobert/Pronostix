ebfc5aea30c1144469230d0b53d70591bb8077c3
mancietfrancois <manciet.francois@hotmail.fr>
2014-12-06 15:20:05 +0100

    UI Commit for Titou
    Basis to start the development of UI (Betting activity).
    Titou is free to work on it

---
+++ core/UI/[Test] BetActivity/Test/app/src/main/java/fr/freud/libraryapptest/Section.java
@@ -0,0 +1,22 @@
+package fr.freud.libraryapptest;
+
+import android.view.View;
+import android.widget.TextView;
+
+import fr.freud.indexedlistview.adapter.Row;
+
+public class Section extends Row {
+	
+	private final String text;
+	private static final int textViewID = R.id.textView1;
+
+    public Section(String text) {
+        this.text = text;
+    }
+
+	public void inflateView(View view) {
+		TextView textView = (TextView) view.findViewById(textViewID);
+		textView.setText(text);
+	}
+
+}
